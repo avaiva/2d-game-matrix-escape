f5ebe1d86d8d41d6817f3d3107e3aed6867dcc82
Eva Kalbfell <e.kalbfell@web.de>
2023-10-31 16:54:15 +0100

    finalize game-screen for logic

--- script/game.js
+++ script/game.js
@@ -4,18 +4,18 @@ class Game {
         this.introScreen = document.getElementById("intro-screen");
         this.gameScreen = document.getElementById("game-screen");
         this.endScreen = document.getElementById("end-screen");
+        this.matrixBody = document.getElementById("matrix-body");
+        this.consoleBody = document.getElementById("console-body");
         this.player = null;
-        this.score = 0;
         this.bugs = null;
         this.errors = null;
+        this.score = 0;
         this.GameIsOver = false;
-
     }
 
     start() {
         this.splashScreen.style.display = "none";
         this.gameScreen.style.display = "block";
-
     }
 
 }
