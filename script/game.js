20a05b51632d94d1888cf5207ca8422bfcb0bc56
Eva Kalbfell <e.kalbfell@web.de>
2023-10-31 20:19:23 +0100

    add player logic move

--- script/game.js
+++ script/game.js
@@ -6,7 +6,14 @@ class Game {
         this.endScreen = document.getElementById("end-screen");
         this.matrixBody = document.getElementById("matrix-body");
         this.consoleBody = document.getElementById("console-body");
-        this.player = null;
+        this.player = new Player(
+            this.matrixBody,
+            0,
+            220,
+            50,
+            50,
+            "/images/player-test.png"
+        );
         this.bugs = null;
         this.errors = null;
         this.score = 0;
@@ -16,6 +23,30 @@ class Game {
     start() {
         this.splashScreen.style.display = "none";
         this.gameScreen.style.display = "block";
+        this.gameLoop();
+    }
+
+    gameLoop() {
+        // console.log("game loop");
+        
+        if (this.GameIsOver) {
+            return;
+        }
+
+        this.update();
+        window.requestAnimationFrame(()=> {
+            this.gameLoop();
+        })
+    }
+
+    update() {
+        // console.log("updated");
+        this.player.move()
+    }
+
+    endGame() {
+        this.GameIsOver = true;
+        this.endScreen.style.display = "block";
     }
 
 }
---
+++ script/player.js
@@ -0,0 +1,42 @@
+class Player {
+    constructor(matrixBody, left, top, width, height, imgSrc) {
+        this.matrixBody = matrixBody;
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+        this.directionX = 0;
+        this.directionY = 0;
+        this.element = document.createElement("img");
+        this.element.src = imgSrc;
+        this.element.style.position = "absolute";
+        this.element.style.left = `${left}px`;
+        this.element.style.top = `${top}px`;
+        this.element.style.width = `${width}px`;
+        this.element.style.height = `${height}px`;
+        this.matrixBody.appendChild(this.element);
+    }
+
+    move() {
+        this.left += this.directionX;
+        this.top += this.directionY;
+        this.element.style.left = `${this.left}px`;
+        this.element.style.top = `${this.top}px`;
+
+        if (this.left < 0) {
+            this.left = 0;
+        }
+
+        if (this.top < 0) {
+            this.top = 0;
+        }
+
+        if (this.left > this.matrixBody.clientWidth - this.width) {
+            this.left = this.matrixBody.clientWidth - this.width;
+        }
+
+        if (this.top > this.matrixBody.clientHeight - this.height) {
+            this.top = this.matrixBody.clientHeight - this.height;
+        }
+    }
+}
